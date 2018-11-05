500905ffda31d8b654aa41592798e725ba8c991e
Ilija Tavchioski <20493797+iletavcioski@users.noreply.github.com>
2018-11-05 12:39:53 +0100

    Add files via upload

---
+++ vsota1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class vsota1
+{
+	public class void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a+b);
+	}
+}
